bae8b00d33a4d61dd910945c7af1a05ebae4541a
gleydson <gleydsonfabricio3@gmail.com>
2025-08-30 15:01:30 -0300

    fix: overflow inf

--- src/main/java/br/ufcg/computacao/floyd-warshall/FloydWarshall.java
+++ src/main/java/br/ufcg/computacao/floyd-warshall/FloydWarshall.java
@@ -1,33 +1,31 @@
 public class FloydWarshall{
 
-    private static final int INFINITO = Integer.MAX_VALUE;
+    private static final int INFINITO = Integer.MAX_VALUE / 2; //overflow em inf + inf, usar inf/2
 
     // utilizando matriz de adjacências
     // método brute-force para achar distancias, calcula todas as possibilidades e atualiza a matriz de distancias
-    public int calcularDistancias(int[][] grafo){
+    // não é guardado o caminhos até ele, apenas a distancia menor possivel
+    public int[][] calcularDistancias(int[][] grafo){
         
         int numVert = grafo.length;
         int[][] distancias = copy(grafo); //não alterar o original
 
-        for (int k = 0; i < numVert; k++) // nó intermediario
+        for (int k = 0; k < numVert; k++) // nó intermediario
             for (int i = 0; i < numVert; i++) // nó de saida
-                for (int j = 0; i < numVert; j++){ // nó de destino
+                for (int j = 0; j < numVert; j++){ // nó de destino
 
                     if (distancias[i][k] != INFINITO && distancias[k][j] != INFINITO) //se os nõs são conectados                        
-                        if (distancia[i][k] + distancia[k][j] < distancia[i][j])
-                            distancia[i][j] = distancia[i][k] + distancia[k][j];
-                } 
-
-
-
-
-
+                        if (distancias[i][k] + distancias[k][j] < distancias[i][j]) //se encontrar um caminho menor, passando pelo intermediario
+                            distancias[i][j] = distancias[i][k] + distancias[k][j]; // atualiza caminho
+                }
+                
+        return distancias;
     }
 
     private int[][] copy(int[][] grafo){
         
         int numVert = grafo.length;
-        int copia = new int[numVert][numVert];
+        int[][] copia = new int[numVert][numVert];
 
         for (int i = 0; i < numVert; i++)
             for (int j = 0; j < numVert; j++)
@@ -37,5 +35,31 @@ public class FloydWarshall{
 
     }
 
+    public void mostrarMatriz(int[][] grafo){
+           
+        for (int saida = 0; saida < grafo.length; saida++){
+            System.out.println("Saindo de: " + saida);
+            for (int chegada = 0; chegada < grafo.length; chegada++){
+                if (grafo[saida][chegada] != 0 && grafo[saida][chegada] != INFINITO)
+                    System.out.println(grafo[saida][chegada] + " unidades até " + chegada);
+            }
+            System.out.println("---");
+        }
+
+    }
+
+    public static void main (String[] args){
+        int[][] matriz = {
+            {0,       3,       10,      INFINITO},
+            {INFINITO, 0,       1,       7},
+            {INFINITO, INFINITO, 0,       2},
+            {INFINITO, INFINITO, INFINITO, 0}
+        };
+
+        FloydWarshall fw = new FloydWarshall();
+        fw.mostrarMatriz(matriz);
+        System.out.println("\n\n");
+        fw.mostrarMatriz(fw.calcularDistancias(matriz));
+    }
 
 }
