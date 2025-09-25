f511b72540652824b0767f60ece55afd76383d9f
Joyce Vitória Nascimento Rodrigues <joycevitoria150@gmail.com>
2025-09-25 14:11:10 -0300

    feat: add BFSBenchmark #1 #18 #3

--- src/jmh/java/br/ufcg/computacao/benchmark/BFSBenchmark.java
+++ src/jmh/java/br/ufcg/computacao/benchmark/BFSBenchmark.java
@@ -1,77 +1,84 @@
-// package br.ufcg.computacao.benchmark;
+package br.ufcg.computacao.benchmark;
 
-// import java.util.List;
-// import java.util.Random;
-// import java.util.concurrent.TimeUnit;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
-// import org.openjdk.jmh.annotations.Benchmark;
-// import org.openjdk.jmh.annotations.BenchmarkMode;
-// import org.openjdk.jmh.annotations.Fork;
-// import org.openjdk.jmh.annotations.Measurement;
-// import org.openjdk.jmh.annotations.Mode;
-// import org.openjdk.jmh.annotations.OutputTimeUnit;
-// import org.openjdk.jmh.annotations.Param;
-// import org.openjdk.jmh.annotations.Scope;
-// import org.openjdk.jmh.annotations.Setup;
-// import org.openjdk.jmh.annotations.State;
-// import org.openjdk.jmh.annotations.Warmup;
-// import org.openjdk.jmh.infra.Blackhole;
-// import br.ufcg.computacao.bfs.BFS;
-// import br.ufcg.computacao.bfs.Grafo;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
 
-// /**
-//  * Benchmark para avaliar o desempenho do BFS
-//  * com diferentes tamanhos e densidades de grafos.
-//  *
-//  * @author Joyce Vitória Nascimento Rodrigues
-//  */
-// @BenchmarkMode(Mode.AverageTime)
-// @OutputTimeUnit(TimeUnit.MILLISECONDS)
-// @State(Scope.Benchmark)
-// @Fork(value = 1)
-// @Warmup(iterations = 2, time = 3)
-// @Measurement(iterations = 3, time = 5)
-// public class BFSBenchmark {
+import br.ufcg.computacao.bfs.BFS;
+import br.ufcg.computacao.bfs.Grafo;
 
-//     @Param({"100", "500", "1000", "5000"})
-//     private int numVertices;
+/**
+ * Benchmark para avaliar o desempenho do algoritmo BFS (Busca em Largura)
+ * com diferentes tamanhos e densidades de grafos.
+ *
+ * @author Joyce Vitória Nascimento Rodrigues
+ */
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+@Fork(value = 1)
+@Warmup(iterations = 2, time = 3)
+@Measurement(iterations = 3, time = 5)
+public class BFSBenchmark {
 
-//     @Param({"0.1", "0.3", "0.5"})
-//     private double densidade;
+    @Param({"100", "500", "1000", "5000"})
+    private int numVertices;
 
-//     private Grafo grafo;
-//     private OrdenacaoTopologica ordenacaoTopologica;
-//     private Random random;
+    @Param({"0.1", "0.3", "0.5"})
+    private double densidade;
 
-//     @Setup
-//     public void setup() {
-//         this.grafo = new Grafo(numVertices);
-//         this.random = new Random(42);
+    private Grafo grafo;
+    private BFS bfs;
+    private Random random;
+    private int verticeOrigem;
 
-//         gerarGrafoAciclicoDirecionado(numVertices, densidade);
+    @Setup
+    public void setup() {
+        this.grafo = new Grafo(numVertices, false); // Grafo não direcionado para BFS
+        this.random = new Random(42);
+
+        gerarGrafoNaoDirecionado(numVertices, densidade);
         
-//         this.ordenacaoTopologica = new OrdenacaoTopologica(grafo);
-//     }
+        // Escolhe um vértice aleatório como origem para a busca
+        this.verticeOrigem = random.nextInt(numVertices);
+        this.bfs = new BFS(grafo);
+    }
 
-//     private void gerarGrafoAciclicoDirecionado(int numVertices, double densidade) {
-//         // garanto que é um DAG: adicionar arestas apenas de vértices menores para maiores
-//         int maxArestas = (numVertices * (numVertices - 1)) / 2;
-//         int numArestas = (int) (maxArestas * densidade);
+    private void gerarGrafoNaoDirecionado(int numVertices, double densidade) {
+        // Para grafo não direcionado: máximo de arestas = n*(n-1)/2
+        int maxArestas = (numVertices * (numVertices - 1)) / 2;
+        int numArestas = (int) (maxArestas * densidade);
         
-//         for (int i = 0; i < numArestas; i++) {
-//             int origem = random.nextInt(numVertices - 1);
-//             int destino = origem + 1 + random.nextInt(numVertices - origem - 1);
-//             if (!grafo.existeAresta(origem, destino)) {
-//                 grafo.adicionarAresta(origem, destino);
-//             }
-//         }
-//     }
+        int arestasAdicionadas = 0;
+        while (arestasAdicionadas < numArestas) {
+            int origem = random.nextInt(numVertices);
+            int destino = random.nextInt(numVertices);
+            
+            if (origem != destino && !grafo.contemAresta(origem, destino)) {
+                // Para grafo não direcionado, adiciona apenas uma aresta (a classe cuida da bidirecionalidade)
+                grafo.adicionarAresta(origem, destino);
+                arestasAdicionadas++;
+            }
+        }
+    }
 
-//     @Benchmark
-//     public void ordenacaoTopologica(Blackhole blackhole) {
-//         List<Integer> resultado = ordenacaoTopologica.ordenar();
-//         blackhole.consume(resultado);
-//     }
-// }
-
-// // repositório: https://github.com/openjdk/jmh
+    @Benchmark
+    public void buscaEmLargura(Blackhole blackhole) {
+        bfs.run(verticeOrigem);
+        blackhole.consume(bfs.getDistancias());
+        blackhole.consume(bfs.getPredecessores());
+        blackhole.consume(bfs.getOrdemVisita());
+    }
+}
